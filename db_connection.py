cb93971baa005065bcb2bf8339b2e6fe246f94c5
Juank1217 <juan.cruz.munoz@correounivalle.edu.co>
2025-03-17 20:43:27 -0500

    Update db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -1,4 +1,6 @@
 import asyncpg
+import os
+
 
 
 class Database:
@@ -11,15 +13,15 @@ class Database:
 
     async def _init_pool(self):
         if not hasattr(self, '_pool'):
-            self._pool = await asyncpg.create_pool(
-                user='AJLN',
-                password='AJLN2025',
-                database='User',
-                host='db',  # Docker service name as host
-                port=5432,
-                min_size=1,
-                max_size=10  # Adjust pool size as needed
-            )
+          self._pool = await asyncpg.create_pool(
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD'),
+            database=os.getenv('DB_NAME'),
+            host=os.getenv('DB_HOST'),
+            port=int(os.getenv('DB_PORT', 5432)),  # valor por defecto si no está definida
+            min_size=1,
+            max_size=10
+        )
 
     async def get_pool(self):
         if not hasattr(self, '_pool'):
